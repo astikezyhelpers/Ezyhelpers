47af8dd7c191fe3711f3c6af4577ed06320f8809
astikezyhelpers <astik.ezyhelpers@gmail.com>
2025-04-10 14:06:19 +0530

    Changed Db configurations.

--- ezyhelpers/ezyhelpers/settings.py
+++ ezyhelpers/ezyhelpers/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yei&qc6j%r@n))l4ge#2q44zc$u6o_j(z$qv#%xkaci#i@^np4'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -77,10 +81,21 @@ WSGI_APPLICATION = 'ezyhelpers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("projectName"),
+        'USER': os.environ.get("projectUser"),
+        'PASSWORD': os.environ.get("userPassword"),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
